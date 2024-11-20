1b5e3c8673f8cc232ad364881ceeade62fbe48c0
nkovacheva <n.kovacheva2020@pmg-pernik.com>
2024-11-20 13:24:07 +0200

    1

---
+++ DogsProjectNew/DogsProjectNew/Models/Dog/DogAllVIewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogsProjectNew.Models.Dog
+{
+    public class DogAllVIewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Age")]
+        public string Age { get; set; }
+
+
+        [Display(Name = "Breed")]
+        public string Breed { get; set; } = null!;
+
+        [Display(Name = "Dog Picture")]
+        public string? Picture { get; set; }
+
+    }
+}
